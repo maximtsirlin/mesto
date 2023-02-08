d79b092247b71ccc4da12e2366d96b707edad593
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-02-08 15:03:10 +0800

    first script

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+
 const aboutButton = document.querySelector('.kusto__edit-button');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.popup__close');
@@ -13,9 +14,28 @@ const handleCloseButtonClick = () => {
 }
 
 
-
 aboutButton.addEventListener('click', handleAboutButtonClick);
 closeButton.addEventListener('click', handleCloseButtonClick);
 
 
 
+
+let myName = document.querySelector ('.kusto__title');
+let aboutPerson = document.querySelector ('.kusto__description');
+
+let formElement = document.querySelector ('.popup');
+
+let nameInput = document.querySelector ('.popup__input-name');
+let jobInput = document.querySelector ('.popup__input-job');
+
+let buttonSave = document.querySelector ('.popup__save');
+
+
+
+function handleFormElementSubmit (evt) {
+    evt.preventDefault();
+    myName.textContent = nameInput.value;
+    aboutPerson.textContent = jobInput.value;
+}
+
+formElement.addEventListener('click', handleFormElementSubmit); 
