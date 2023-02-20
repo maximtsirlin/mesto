07885b35c3fb7bb735b27f70fee553e81e762b4f
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-02-20 17:21:13 +0800

    new button

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,11 @@ const popup = document.querySelector('.popup');
 const buttonClose = popup.querySelector('.popup__close');
 const title = document.querySelector('.profile__title');
 const description = document.querySelector('.profile__description');
+/* открытие попапа для кнопки add */
+const aboutAddButton = document.querySelector('.profile__button') /* querySelectorAll выделяем кнопку */
+const add = document.querySelector('.add');
+/* закрытие попапа для кнопки add */
+const closeAddButton = add.querySelector('.add__close');
 
 let formElement = document.querySelector('.popup__input-container');
 let nameInput = document.querySelector('.popup__form_input_name');
@@ -27,6 +32,20 @@ const handleFormElementSubmit = (evt) => {
     closePopup();
 }
 
+
+const handleAddButtonClick = () => {
+    add.classList.add('add_opened');
+} /* функция c добавлением булевого модификатора*/
+
+const handleСloseAddButtonClick = () => {
+    add.classList.remove('add_opened');
+} /* функция закрытия*/
+
+
+
 formElement.addEventListener('submit', handleFormElementSubmit);
 buttonAbout.addEventListener('click', handleAboutButtonClick);
 buttonClose.addEventListener('click', closePopup);
+
+aboutAddButton.addEventListener('click', handleAddButtonClick); /* функция открытия */
+closeAddButton.addEventListener('click', handleСloseAddButtonClick); /* функция закрытия */
