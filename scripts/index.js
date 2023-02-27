0113550b1526e0d1442bc0b0865a0ae3607c1837
Maxim Tsirlin <maximtsirlin@gmail.com>
2023-02-28 02:15:03 +0800

    fix bags

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ const popupEdit = document.querySelector('.popup_edit');
 const profileTitle = document.querySelector('.profile__title'); /* выделяем заголовок */
 const profileDescription = document.querySelector('.profile__description');
 
-const placeAddButton = document.querySelector('.profile__add-button') 
+const placeAddButton = document.querySelector('.profile__add-button')
 
 const popupAdd = document.querySelector('.popup_add');
 const popupAddSaveButton = popupAdd.querySelector('.popup__save'); /* выделяем кнопку создать */
@@ -17,16 +17,14 @@ const popupImageImg = popupImage.querySelector('.popup__img')
 const nameImgClicked = popupImage.querySelector('.popup__figcaption'); /* выделяем заголовок попапа */
 
 
-const profileEditForm = document.querySelector('.popup__input-container');
+const profileEditForm = document.querySelector('.popup__input-container');  /* нахожу форму попапа 1 */
 const profileEditNameInput = document.querySelector('.popup__form_input_name');
 const profileEditJobInput = document.querySelector('.popup__form_input_job');
 
-const addPlaceForm = document.querySelector('.popup__input-add'); /* нахожу форму */
-
+const addPlaceForm = document.querySelector('.popup__input-add'); /* нахожу форму попапа 2 */
 
 
 const allPopups = document.querySelectorAll('.popup') /* нашли все попапы */
-
 
 
 const openPopup = (popup) => {
@@ -40,32 +38,32 @@ const closePopup = (popup) => {
 allPopups.forEach((popup) => { /* пробегаемся по списку попапов ь */
   const closeBtn = popup.querySelector('.popup__close')
   if (closeBtn) {
-    closeBtn.addEventListener('click', () => {closePopup(popup)}) /* навешиваем на кнопку закрытия обработчик */
+    closeBtn.addEventListener('click', () => { closePopup(popup) }) /* навешиваем на кнопку закрытия обработчик */
   }
 })
 
 const openProfilePopup = () => {
-    profileEditNameInput.value = profileTitle.textContent;
-    profileEditJobInput.value = profileDescription.textContent;
-    openPopup(popupEdit)
+  profileEditNameInput.value = profileTitle.textContent;
+  profileEditJobInput.value = profileDescription.textContent;
+  openPopup(popupEdit) /* открытие edit */
 }
 
 const handleProfileFormSubmit = (evt) => {
-    evt.preventDefault();
-    profileTitle.textContent = profileEditNameInput.value; /* добавляем его к инпуту */
-    profileDescription.textContent = profileEditJobInput.value;
-    closePopup(popupEdit)
+  evt.preventDefault();
+  profileTitle.textContent = profileEditNameInput.value; /* добавляем его к инпуту */
+  profileDescription.textContent = profileEditJobInput.value;
+  closePopup(popupEdit) /* закрытие кнопки сохранить */
 }
 
-const closePopupEdit = () =>  {
-  closePopup(popupEdit)
-}
+
 
 const OpenAddPlace = () => {
-    openPopup(popupAdd)
+  openPopup(popupAdd)
 } /* функция c добавлением булевого модификатора*/
 
-
+const closePopupEdit = () => {
+  closePopup(popupEdit)
+}
 
 
 
@@ -74,41 +72,40 @@ profileEditButton.addEventListener('click', openProfilePopup);
 
 placeAddButton.addEventListener('click', OpenAddPlace); /* функция открытия */
 
-
 ///////////////////////////////////////////////////////////////////
 
 const initialCards = [
-    {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-  ]; 
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
 const cardsListContainer = document.querySelector('.cards'); /* нахожу контейнер куда рендерить */
 const placeCardTemplate = document.getElementById('cards__template').content;
 
 const deleteCard = (evt) => {
-    evt.target.closest('.cards__cell').remove();
+  evt.target.closest('.cards__cell').remove();
 }
 
 const openPopupImage = (title, link) => {
@@ -120,34 +117,34 @@ const openPopupImage = (title, link) => {
 
 
 const getItemElement = (title, link) => {
-    const newItemElement = placeCardTemplate.cloneNode(true); /* клонирую карточку */
+  const newItemElement = placeCardTemplate.cloneNode(true); /* клонирую карточку */
 
-    const newItemTitle = newItemElement.querySelector('.cards__description'); /* берем заголовок */
-    newItemTitle.textContent = title;  /* вставляем в карточку заголовок переданный в аргументах */
+  const newItemTitle = newItemElement.querySelector('.cards__description'); /* берем заголовок */
+  newItemTitle.textContent = title;  /* вставляем в карточку заголовок переданный в аргументах */
 
-    const newItemImage = newItemElement.querySelector('.cards__item'); /* берем картинку */
-    newItemImage.src = link; /* вставляем ссылку */
-    newItemImage.alt = title; /* вставляем описание */
-    newItemImage.addEventListener('click', () => { openPopupImage(title, link)})
+  const newItemImage = newItemElement.querySelector('.cards__item'); /* берем картинку */
+  newItemImage.src = link; /* вставляем ссылку */
+  newItemImage.alt = title; /* вставляем описание */
+  newItemImage.addEventListener('click', () => { openPopupImage(title, link) })
 
-    const deleteButton = newItemElement.querySelector('.cards__delete');  /* кнопка удаления */
-    deleteButton.addEventListener('click', deleteCard)
+  const deleteButton = newItemElement.querySelector('.cards__delete');  /* кнопка удаления */
+  deleteButton.addEventListener('click', deleteCard)
 
-    const likeButton = newItemElement.querySelector('.cards__button'); /* кнопка лайка */
-    likeButton.addEventListener("click", (event) => {
-      event.target.classList.toggle('cards__button-active');
-    });
+  const likeButton = newItemElement.querySelector('.cards__button'); /* кнопка лайка */
+  likeButton.addEventListener("click", (event) => {
+    event.target.classList.toggle('cards__button-active');
+  });
 
 
-    return newItemElement;
+  return newItemElement;
 }  /* создает элемент, стрелочная функция которая принимает в себя заголовок и ссылку */
 
 const generatePlaceCard = (title, link) => {
-  const newItem = getItemElement(title, link) 
+  const newItem = getItemElement(title, link)
   cardsListContainer.prepend(newItem)  /* вставляем в начало контейнера */
 }
 
-initialCards.forEach((place)=> {  /* пробегаемся по массиву мест и генерируем карточки  */
+initialCards.forEach((place) => {  /* пробегаемся по массиву мест и генерируем карточки  */
   generatePlaceCard(place.name, place.link)
 })
 
@@ -160,9 +157,9 @@ const addNewCard = (evt) => {
   if (title, link) {
     generatePlaceCard(title, link);
   }
-  evt.target.reset();
-
-}
+  evt.target.reset()
+  closePopup(popupAdd) 
+};
 
 addPlaceForm.addEventListener('submit', addNewCard)
 
